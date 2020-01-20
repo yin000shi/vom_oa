eede4efb8d06a7c2b9c9c2a8096d540adf7a8a4f
yinshi <yin000shi@126.com>
2020-01-21 00:03:19 +0800

    change to mysql

--- vom_oa/settings.py
+++ vom_oa/settings.py
@@ -78,14 +78,12 @@ WSGI_APPLICATION = 'vom_oa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE':'django.db.backends.mysql',
-        # 'NAME': 'database-1',
-        # 'USER': 'root',
-        # 'PASSWORD': 'yinshi871024',
-        # 'HOST': 'database-1.csrhb7ptznwj.ap-southeast-1.rds.amazonaws.com',
-        # 'PORT': '3306',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vom_oa',
+        'USER': 'root',
+        'PASSWORD': '887207610ae3a438',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -110,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
